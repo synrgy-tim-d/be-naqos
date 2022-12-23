38ea7a6abe24ab0dd281e8b92b834a328ea2ca55
Philip Bagus Kristianto <bagus09051998@gmail.com>
2022-12-23 22:01:33 +0700

    23/12/2022 | Add Jwt Utils

---
+++ src/main/java/com/binar/kelompokd/utils/JwtUtils.java
@@ -0,0 +1,52 @@
+package com.binar.kelompokd.utils;
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwt(Authentication auth) {
+        return Jwts.builder()
+                .setSubject(...)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime()+ jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            LOG.error("Invalid JWT Signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            LOG.error("Invalid JWT Token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            LOG.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            LOG.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOG.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+}
