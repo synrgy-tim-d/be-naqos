a9d9fdc56a2f399741e63121a8f6b30a9f5d1b20
Muhajir <aryamuhajir888@gmail.com>
2023-01-05 22:16:46 +0700

    Auth login, register with otp, forgot password,

--- src/main/java/com/binar/kelompokd/BackendApplication.java
+++ src/main/java/com/binar/kelompokd/BackendApplication.java
@@ -1,7 +1,9 @@
 package com.binar.kelompokd;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@OpenAPIDefinition
 
 @SpringBootApplication
 public class BackendApplication {
---
+++ src/main/java/com/binar/kelompokd/models/dto/ResetPasswordDTO.java
@@ -0,0 +1,12 @@
+package com.binar.kelompokd.models.dto;
+
+import lombok.Data;
+
+@Data
+
+public class ResetPasswordDTO {
+    public String email;
+
+    public String otp;
+    public String newPassword;
+}
