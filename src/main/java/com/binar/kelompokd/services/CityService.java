1c2a3ba80e32fd66d12cfca3429d25151311b0e3
Isa Randra <isarandra@yahoo.com>
2023-01-14 14:15:58 +0700

    crud endpoints city

---
+++ src/main/java/com/binar/kelompokd/services/CityService.java
@@ -0,0 +1,13 @@
+package com.binar.kelompokd.services;
+
+import com.binar.kelompokd.models.entity.City;
+
+import java.util.List;
+
+public interface CityService {
+    City save(City city);
+    City getCityById(Integer id);
+    List<City> getAllCities();
+    City update(Integer id, City city);
+    void delete(Integer id);
+}
