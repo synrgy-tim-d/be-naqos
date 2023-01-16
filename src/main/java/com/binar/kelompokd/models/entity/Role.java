d1bf030210d992401ccc0a96ab813e8bb89826c2
Isa Randra <isarandra@yahoo.com>
2023-01-16 15:35:31 +0700

    uncomment for merging

--- src/main/java/com/binar/kelompokd/models/entity/Role.java
+++ src/main/java/com/binar/kelompokd/models/entity/Role.java
@@ -1,26 +1,26 @@
-//package com.binar.kelompokd.models.entity;
-//
-//import com.binar.kelompokd.enums.ERole;
-//import com.binar.kelompokd.models.DateModel;
-//import lombok.Data;
-//
-//import javax.persistence.*;
-//import javax.validation.constraints.NotNull;
-//import java.io.Serializable;
-//
-//@Entity
-//@Data
-//@Table(name = "t_roles")
-//public class Role extends DateModel implements Serializable {
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.AUTO)
-//  private Integer id;
-//
-//  @Enumerated(EnumType.STRING)
-//  @Column(length = 20)
-//  @NotNull
-//  private ERole name;
-//
-//  @NotNull
-//  private Boolean isActive = false;
-//}
+package com.binar.kelompokd.models.entity;
+
+import com.binar.kelompokd.enums.ERole;
+import com.binar.kelompokd.models.DateModel;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "t_roles")
+public class Role extends DateModel implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(length = 20)
+  @NotNull
+  private ERole name;
+
+  @NotNull
+  private Boolean isActive = false;
+}
