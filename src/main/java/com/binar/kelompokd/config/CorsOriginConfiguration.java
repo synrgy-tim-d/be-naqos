41047f1247b226b298e6bd253a538f5730f108eb
Philip Bagus Kristianto <bagus09051998@gmail.com>
2023-01-18 18:31:07 +0700

    18/01/23 | Add Credential to access Swagger

--- src/main/java/com/binar/kelompokd/config/CorsOriginConfiguration.java
+++ src/main/java/com/binar/kelompokd/config/CorsOriginConfiguration.java
@@ -23,11 +23,11 @@ public class CorsOriginConfiguration implements Filter {
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain filter) throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
     HttpServletRequest request = (HttpServletRequest) req;
-    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Origin", "https://app.swaggerhub.com");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, X-Auth-Token, Data");
-    // response.setHeader("Access-Control-Allow-Headers", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
 
     if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
       response.setStatus(HttpServletResponse.SC_OK);
