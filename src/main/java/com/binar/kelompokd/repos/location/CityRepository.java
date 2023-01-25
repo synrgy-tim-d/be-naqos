5d9d19be1ffdae7a6ebd3106a4b0863fca3070b0
Isa Randra <isarandra@yahoo.com>
2023-01-26 05:25:11 +0700

    add find all kosts with filter by city

--- src/main/java/com/binar/kelompokd/repos/location/CityRepository.java
+++ src/main/java/com/binar/kelompokd/repos/location/CityRepository.java
@@ -2,10 +2,17 @@ package com.binar.kelompokd.repos.location;
 
 import com.binar.kelompokd.models.entity.location.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Integer> {
   City findByCity(String city);
   City getCityById(Integer id);
+  @Query(
+          nativeQuery = true,
+          value = "select * from t_setup_city where city=:name"
+  )
+  City getCityByName(@Param("name") String name);
 }
